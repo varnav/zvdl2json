5045bd7bbe7f67c0935423f70586b246621acaf7
Evgeny Varnavskiy <varnavruz@gmail.com>
2021-10-04 12:24:00 -0400

    Move all unnecessary feature to different branch

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
 # zvdl2json
 
-This daemon will attempt to receive ZMQ input from [dumpvdl2](https://github.com/szpajder/dumpvdl2/) and do some manipulations.
-Right now it attempts to output [vdlm2dec](https://github.com/TLeconte/vdlm2dec) compatible JSON for feeding to [airframes.io](https://airframes.io) and dump some interesting information to CSV files.
-
-## Features
-
-* Works on Windows & Linux, works on Raspberry Pi.
-* Read and save [ADS-C](https://code7700.com/surveillance_ads-c.htm) positions to csv file
-* Plot those positions on map
-* Feed to airframes.io
-* Attempt to find and save interesting messages
+This daemon will receive ZMQ input from [dumpvdl2](https://github.com/szpajder/dumpvdl2/) and output [vdlm2dec](https://github.com/TLeconte/vdlm2dec) compatible JSON for feeding to [airframes.io](https://airframes.io).
 
 ## Install
 
 ```shell
-sudo -i
-apt install -y python3-pip
 cd /usr/src
 git clone https://github.com/varnav/zvdl2json.git
 cd zvdl2json
@@ -25,13 +14,8 @@ cp zvdl2json.py /opt/zvdl2json
 chmod +x /opt/zvdl2json/zvdl2json.py
 cp zvdl2json.service /etc/systemd/system/zvdl2json.service
 systemctl daemon-reload
-systemctl enable zvdl2json
-```
-
-## Run 
-
-```shell
-systemctl start zvdl2json && systemctl status zvdl2json
+systemctl enable zvdl2json --now
+systemctl status zvdl2json
 ```
 
 # Update
@@ -46,21 +30,10 @@ Change station ID to your own:
 `KJFK` - nearest airport
 
 ```shell
-dumpvdl2 --rtlsdr 0 --station-id JD-KJFK-VDL2 --msg-filter all,-avlc_s,-acars_nodata,-gsif,-x25_control,-idrp_keepalive,-esis --output decoded:json:zmq:mode=client,endpoint=tcp://127.0.0.1:5555 136650000 136700000 136800000 136975000
+dumpvdl2 --rtlsdr 0 --station-id JD-KJFK-VDL2 --bs-db /root/BaseStation.sqb --msg-filter all,-avlc_s,-acars_nodata,-gsif,-x25_control,-idrp_keepalive,-esis --output decoded:json:zmq:mode=client,endpoint=tcp://127.0.0.1:5555 136650000 136700000 136800000 136975000
 ```
 
-You may use serial of the SDR dongle instead of `0`. Use frequencies depending on region:
-
-```
-136.650 USA (ARINC)
-136.700 USA (ARINC)
-136.800 USA (SITA)
-136.725 Europe (ARINC)
-136.775 Europe (SITA)
-136.825 Europe (ARINC)
-136.875 Europe (SITA)
-136.975 Worldwide (SITA & ARINC)
-```
+You may use serial of the SDR dongle instead of `0`.
 
 ## Installation of dumpvdl2
 
@@ -73,11 +46,15 @@ cd dumpvdl2
 mkdir build
 cd build
 cmake ../
-make
+make -j3
 make install
 cd ..
 cp etc/dumpvdl2.service /etc/systemd/system/
 cp etc/dumpvdl2 /etc/default/
+systemctl daemon-reload
+cd ~
+wget https://github.com/varnav/BaseStation.sqb/releases/download/latest/BaseStation.sqb.tar.xz
+tar xf BaseStation.sqb.tar.xz
 ```
 
 Now edit parameters:
@@ -89,20 +66,6 @@ systemctl start dumpvdl2 && systemctl status dumpvdl2
 ```
 
 See example on top for options
-
-## Plot collected positions from CSV
-
-```shell
-./ads-c_plot.py ~/zvdl2json/2021-08-28_positions.csv
-```
-
-## Note on optimal antenna length
-
-137 MHz means 1/4 antenna must be 521 mm long.
-
-# TODO
-
-* MongoDB or ElasticSearch instead of dumping to text/csv
 
 ## See also
 
--- zvdl2json.py
+++ zvdl2json.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 
-import csv
 import datetime
 import json
-import os
-import traceback
-from pathlib import Path
-from sty import fg
-
 import zmq
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 verbose = False
-watch_regex = 'ACCIDENT|COCKPIT|COMPUTER|CABIN|CAPT|COLLISION|CAUTION|DISPATCH|DIVER|DRUG|EMERGENCY|FAILED|FIRE|GUESS|HELP|PHONE|IPAD|IFE|INCIDENT|INFORM|KNOW|KEEPING|LASER|LOOK|MASK|MARSHALL|MEDICAL|OXYGEN|ODD|PILOT|PAX|PLZ|PLEASE|POLICE|PHONE|RIDE|RELEASE|REPORTED|RETRACTING|ROUTING|RESET|RETURN|SHIP|SMOKE|SHIP|SICK|SUGGEST|THREAT|TAPS|TRAFFIC|USB|VOMIT|WHAT|WHY|WINDOW|WEATHER|WRN|WOULD|FAULT|DISP|LEO|GSC|NEED|LIFE|URGENT|FREE|TEXT|CAPT|SIGHT'
-
-os.system("")
-
-def bell():
-    if os.name == 'nt':
-        import winsound
-        winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
 
 
 def main():
@@ -31,20 +17,10 @@ def main():
     print(datetime.datetime.now(), 'ZMQ listening at:', binding)
     s.setsockopt_string(zmq.SUBSCRIBE, '')
 
-    # Get workdir
-    workdir = Path.home() / 'zvdl2json'
-    Path(workdir).mkdir(parents=True, exist_ok=True)
-    print(datetime.datetime.now(), 'Dumping data to:', workdir)
-
     while True:
         data = s.recv_json()
         if verbose:
             print(json.dumps(data, indent=4))
-
-        # Save everything
-        filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_all.json'
-        with open(workdir / filename, 'a', encoding='utf-8') as f:
-            f.write(json.dumps(data) + "\n")
 
         # Now we'll remap ZMQ input from dumpvdl2 to vdlm2dec compatible JSON, example:
         # {"timestamp":1630081049.174413,"station_id":"EV-KPTK0-VDL2","channel":0,"freq":136.650,"icao":10871946,"toaddr":1127706,"is_response":0,"is_onground":0,"mode":"2","label":"H1","block_id":"3","ack":"!","tail":"N479WN","flight":"WN6197","msgno":"D09A","text":"#DFB76401\r\n02E27KMSPKBWI\r\nN42395W08427716173898M054263024G000X210030PSZ\r\n"}
@@ -82,7 +58,7 @@ def main():
             # if verbose:
             #     print(json.dumps(flat, indent=2, sort_keys=True))
 
-            # Send this via UDP to airframes.io
+            # Send this via UDP to feed.acars.io
             try:
                 import socket
                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
@@ -96,129 +72,6 @@ def main():
         except KeyError:
             print("Conversion error")
             traceback.print_exc()
-
-        # Dump all non-empty ACARS messages to CSV
-        if 'acars' in data['vdl2']['avlc']:
-            msgtime = datetime.datetime.utcfromtimestamp(data['vdl2']['t']['sec']).isoformat() + 'Z'
-            print(msgtime, data['vdl2']['avlc']['acars']['reg'].replace('.', ''), "===>\n", data['vdl2']['avlc']['acars']['msg_text'])
-            filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_messages.csv'
-            with open(workdir / filename, 'a', encoding='utf-8', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([msgtime, data['vdl2']['avlc']['acars']['reg'].replace('.', ''), data['vdl2']['avlc']['acars']['msg_text']])
-
-        # Dump all non-empty non-ACARS messages to CSV
-        if 'text' in data['vdl2']:
-            msgtime = datetime.datetime.utcfromtimestamp(data['vdl2']['t']['sec']).isoformat() + 'Z'
-            print(msgtime, data['vdl2']['reg'], "===>\n", data['vdl2']['text'])
-            filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_messages.csv'
-            with open(workdir / filename, 'a', encoding='utf-8', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([msgtime, data['vdl2']['flight'], data['vdl2']['text']])
-            # Word watchist
-            if bool(re.search(watch_regex, data['vdl2']['text'])):
-                print('Interesting information found')
-                bell()
-                print(fg.yellow + data['vdl2']['text'] + fg.rs)
-                filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_interesting.json'
-                with open(workdir / filename, 'a', encoding='utf-8') as f:
-                    f.write(json.dumps(data) + "\n")
-
-        # Look for interesting things in ACARS
-
-        import re
-
-        if 'acars' in data['vdl2']['avlc']:
-
-            # ADS-C
-
-            try:
-                if 'arinc622' in data['vdl2']['avlc']['acars']:
-                    if data['vdl2']['avlc']['acars']['arinc622']['msg_type'] == 'adsc_msg':
-                        print('ADS-C message detected, saving position')
-                        # Reassemble time
-                        t = datetime.datetime.utcfromtimestamp(data['vdl2']['t']['sec'])
-                        mins = int(int(data['vdl2']['avlc']['acars']['arinc622']['adsc']['tags'][1]['basic_report']['ts_sec']) / 60)
-                        ts = t.replace(minute=mins).isoformat() + 'Z'  # ISO time in UTC (Zulu)
-                        tail = data['vdl2']['avlc']['acars']['reg'].replace('.', '')
-                        lat = data['vdl2']['avlc']['acars']['arinc622']['adsc']['tags'][1]['basic_report']['lat']
-                        lon = data['vdl2']['avlc']['acars']['arinc622']['adsc']['tags'][1]['basic_report']['lon']
-                        alt = data['vdl2']['avlc']['acars']['arinc622']['adsc']['tags'][1]['basic_report']['alt']
-                        filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_positions.csv'
-                        with open(workdir / filename, 'a', encoding='utf-8', newline='') as f:
-                            writer = csv.writer(f)
-                            writer.writerow([ts, tail, lat, lon, alt])
-
-            except KeyError:
-                print("ADS-C decode error")
-                traceback.print_exc()
-
-            # VDL position
-
-            # try:
-            #     if 'xid' in data['vdl2']['avlc']:
-            #         if 'ac_location' in data['vdl2']['avlc']['xid']['vdl_params']:
-            #             print(colored("VLD position", 'yellow'))
-            #             lat = data['vdl2']['avlc']['xid']['vdl_params']['ac_position']['lat']
-            #
-            #
-            #             filename = workdir + datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_adsc.csv'
-            #             with open(workdir / filename, 'a', encoding='utf-8') as f:
-            #                 writer = csv.writer(f)
-            #                 writer.writerow([ts, tail, lat, lon, alt])
-            #
-            # except KeyError as err:
-            #     print("VLD position decode error")
-            #     traceback.print_exc()
-
-            # regex = r'.AT1.'
-            # # FANS - AT1 – CPDLC – aircraft controller directions or response from pilot (can be input or output)
-            # if bool(re.search(regex, data['vdl2']['avlc']['acars']['msg_text'])):
-            #     print('AT1 message')
-            #     filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + 'at1.txt'
-            #     with open(workdir / filename, 'a', encoding='utf-8') as f:
-            #         f.write(data['vdl2']['avlc']['acars']['msg_text'] + "\n")
-
-            # regex = r'.ADS.'
-            # # ADS - ADS-C position report
-            # if bool(re.search(regex, data['vdl2']['avlc']['acars']['msg_text'])):
-            #     print('ADS message')
-            #     filename = workdir + datetime.datetime.utcnow().strftime("%Y-%m-%d") + 'ads.txt'
-            #     with open(filename, 'a', encoding='utf-8') as f:
-            #         f.write(data['vdl2']['avlc']['acars']['msg_text'] + "\n")
-
-            # Dump interesting ACARS labels
-            # https://www.universal-radio.com/catalog/decoders/acarsweb.pdf
-            regex = r'00|7A|A7|B7|C\d|M\d|Q.'
-            if bool(re.search(regex, data['vdl2']['avlc']['acars']['label'])):
-                print(data['vdl2']['avlc']['acars']['msg_text'])
-                filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_interesting.json'
-                with open(workdir / filename, 'a', encoding='utf-8') as f:
-                    f.write(json.dumps(data) + "\n")
-
-            # Coordinates search
-            regex = r'[S,N] \d\d\.\d{3,4} [W,E] \d\d\d\.\d\d|FPO\/\d\d|[N,S]\d\d\d\d.\d,[W,E]\d\d\d\d\d.\d|POS[N,S]'
-            if bool(re.search(regex, data['vdl2']['avlc']['acars']['msg_text'])):
-                print('Coordinates found in message')
-                # print(data['vdl2']['avlc']['acars']['msg_text'])
-                filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_position_messages.json'
-                with open(workdir / filename, 'a', encoding='utf-8') as f:
-                    f.write(json.dumps(data) + "\n")
-
-            # Word watchist
-            if bool(re.search(watch_regex, data['vdl2']['avlc']['acars']['msg_text'])):
-                print('Interesting information found')
-                bell()
-                print(fg.YELLOW + data['vdl2']['avlc']['acars']['msg_text'] | fg.rs)
-                filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_interesting.json'
-                with open(workdir / filename, 'a', encoding='utf-8') as f:
-                    f.write(json.dumps(data) + "\n")
-
-                # Parse position and save to file
-                # filename = datetime.datetime.utcnow().strftime("%Y-%m-%d") + '_other_positions.json'
-                # with open(workdir / filename, 'a', encoding='utf-8') as f:
-                #     writer = csv.writer(f)
-                #     lat = data['vdl2']['avlc']['acars']['msg_text']
-                #     writer.writerow([data['vdl2']['avlc']['acars']['reg'], lat, lon, alt])
 
 
 if __name__ == "__main__":
